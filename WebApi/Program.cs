54d0ae4dac2769c50229fc78c22d2c35e5be2eca
Chukwu Sydney <chukwusydney061@gmail.com>
2026-03-21 01:04:49 +0100

    Add Serilog to SQL Database

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using Persistance;
 using Persistance.Seeds;
+using Serilog;
 using System.Text;
 using WebApi.Middlewares;
 using WebApi.Services;
@@ -12,92 +13,115 @@ using WebApi.SharedServices;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Information()
+            .WriteTo.Console()
+            .WriteTo.MSSqlServer(
+                connectionString: builder.Configuration.GetConnectionString("DefaultConnection"),
+                sinkOptions: new Serilog.Sinks.MSSqlServer.MSSqlServerSinkOptions { TableName = "SerilogEvents", AutoCreateSqlTable = true })
+            //.WriteTo.File("logs/myapp.txt", rollingInterval: RollingInterval.Day)
+            .CreateLogger();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerExtensions();
 
-builder.Services.AddApplication();
-builder.Services.AddPersistance(builder.Configuration);
-builder.Services.AddInfrastructure();
-builder.Services.AddScoped<IAuthenticatedUser, AuthenticatedUser>();
-builder.Services.AddHttpContextAccessor();
+Log.Information("Hello, world!");
 
-builder.Services.AddAuthentication(options =>
+//Add services to the container.
+try
 {
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(o =>
-{
-    o.RequireHttpsMetadata = false;
-    o.SaveToken = false;
-    o.TokenValidationParameters = new TokenValidationParameters
+    builder.Host.UseSerilog();
+    builder.Services.AddControllers();
+    //Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerExtensions();
+
+    builder.Services.AddApplication();
+    builder.Services.AddPersistance(builder.Configuration);
+    builder.Services.AddInfrastructure();
+    builder.Services.AddScoped<IAuthenticatedUser, AuthenticatedUser>();
+    builder.Services.AddHttpContextAccessor();
+
+    builder.Services.AddAuthentication(options =>
     {
-        ValidateIssuer = true,
-        ValidateIssuerSigningKey = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ClockSkew = TimeSpan.Zero,
-        ValidIssuer = builder.Configuration["JWTSettings:Issuer"],
-        ValidAudience = builder.Configuration["JWTSettings:Audience"],
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWTSettings:Key"]))
-
-    };
-    o.Events = new JwtBearerEvents()
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    }).AddJwtBearer(o =>
     {
-        OnAuthenticationFailed = c =>
+        o.RequireHttpsMetadata = false;
+        o.SaveToken = false;
+        o.TokenValidationParameters = new TokenValidationParameters
         {
-            c.NoResult();
-            c.Response.StatusCode = 500;
-            c.Response.ContentType = "text/plain";
-            return c.Response.WriteAsync(c.Exception.ToString());
-        },
-        OnChallenge = context =>
+            ValidateIssuer = true,
+            ValidateIssuerSigningKey = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ClockSkew = TimeSpan.Zero,
+            ValidIssuer = builder.Configuration["JWTSettings:Issuer"],
+            ValidAudience = builder.Configuration["JWTSettings:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWTSettings:Key"]))
+
+        };
+        o.Events = new JwtBearerEvents()
         {
-            context.HandleResponse();
-            context.Response.StatusCode = 401;
-            context.Response.ContentType = "text/plain";
-            return context.Response.WriteAsync("User Unauthorised");
-        },
-        OnForbidden = context =>
-        {
-            context.Response.StatusCode = 403;
-            context.Response.ContentType = "text/plain";
-            return context.Response.WriteAsync("User Forbidden due to insufficient permission");
-        },
-    };
+            OnAuthenticationFailed = c =>
+            {
+                c.NoResult();
+                c.Response.StatusCode = 500;
+                c.Response.ContentType = "text/plain";
+                return c.Response.WriteAsync(c.Exception.ToString());
+            },
+            OnChallenge = context =>
+            {
+                context.HandleResponse();
+                context.Response.StatusCode = 401;
+                context.Response.ContentType = "text/plain";
+                return context.Response.WriteAsync("User Unauthorised");
+            },
+            OnForbidden = context =>
+            {
+                context.Response.StatusCode = 403;
+                context.Response.ContentType = "text/plain";
+                return context.Response.WriteAsync("User Forbidden due to insufficient permission");
+            },
+        };
 
 
-});
+    });
 
-var app = builder.Build();
+    var app = builder.Build();
 
-//Add this block - it waits for the app to be ready before seeding
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-    // Call your seed methods here instead of in ServiceExtensions
-    await DefaultRoles.SeedRolesAsync(services);
-    await DefaultUsers.SeedUsersAsync(services);
+    //Add this block - it waits for the app to be ready before seeding
+    using (var scope = app.Services.CreateScope())
+    {
+        var services = scope.ServiceProvider;
+        // Call your seed methods here instead of in ServiceExtensions
+        await DefaultRoles.SeedRolesAsync(services);
+        await DefaultUsers.SeedUsersAsync(services);
+    }
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthentication();
+
+    app.UseAuthorization();
+
+    app.UseMiddleware<ErrorHandlerMiddleware>();
+
+    app.MapControllers();
+
+    app.Run();
 }
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+catch(Exception ex)
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    Log.Error(ex, "Something went wrong");
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthentication();
-
-app.UseAuthorization();
-
-app.UseMiddleware<ErrorHandlerMiddleware>();
-
-app.MapControllers();
-
-app.Run();
+finally
+{
+    await Log.CloseAndFlushAsync();
+}
